79b265cbb6d965c32377b0dc427c22c9736e9131
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-15 22:33:19 +0200

    add tests

---
+++ test/Primitives/PrimitiveTest.php
@@ -0,0 +1,37 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+
+    use Chameleon\Primitives\Primitive;
+    use Chameleon\Colors\RGBColor;
+    use Chameleon\Vector2;
+
+    final class PrimitiveTest extends TestCase {
+
+        private $primitive;
+
+        protected function setUp() {
+            $this -> primitive = $this -> getMockForAbstractClass(Primitive::class);
+        }
+
+        public function testSetGetPosition() {
+            $pos = new Vector2(10, 20);
+
+            $this -> primitive -> setPosition($pos);
+            $this -> assertEquals($pos, $this -> primitive -> getPosition());
+        }
+
+        public function testSetGetBorder() {
+            $border = new Border(5, new RGBColor(255, 0, 0));
+
+            $this -> primitive -> setBorder($border);
+            $this -> assertEquals($border, $this -> primitive -> getBorder());
+        }
+
+        public function testSetGetBackground() {
+            $background = new SimpleBackground(new RGBColor(128, 64, 32));
+
+            $this -> primitive -> setBackground($background);
+            $this -> assertEquals($background, $this -> primitive -> getBackground());
+        }
+    }
+?>
