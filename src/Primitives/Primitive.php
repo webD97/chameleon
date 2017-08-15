df87900f653a73cf13186426585a7fcad7fc13c8
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-15 22:17:14 +0200

    fix class path

--- src/Primitives/Primitive.php
+++ src/Primitives/Primitive.php
@@ -1,12 +1,14 @@
 <?php
     namespace Chameleon\Primitives;
 
+    use Chameleon\Vector2;
+
     abstract class Primitive implements IPrimitive {
         protected $point;
 
         public abstract function draw($imageResource);
 
-        public function setPosition(\Vector2 $point) {
+        public function setPosition(Vector2 $point) {
             $this -> point = $point;
         }
     }
