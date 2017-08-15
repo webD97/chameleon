0423be42abd60cfebbed0d1a22cd294110eaccd5
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-15 16:45:52 +0200

    start phpdoc

--- src/Colors/HSVColor.php
+++ src/Colors/HSVColor.php
@@ -3,6 +3,12 @@
 
     require_once("Color.php");
 
+    /**
+    * Color Class for HSV colors
+    *
+    * Represents a color based on the HSV (hue, saturation, value) system
+    * @api
+    */
     class HSVColor extends Color {
         private $hue;
         private $saturation;
@@ -12,6 +18,10 @@
         private $green;
         private $blue;
 
+        /**
+        * Class constructor
+        * @param int $hue Hue value [0, 360]
+        */
         public function __construct(int $hue, float $saturation, float $value) {
             $this -> hue = $hue;
             $this -> saturation = $saturation;
@@ -24,30 +34,73 @@
             return "hsv(" . $this -> hue . ", " . $this -> saturation . ", " . $this -> value . ")";
         }
 
+        /**
+        * Get hue value
+        *
+        * Returns the hue value of this color
+        * @return int Hue value [0, 360]
+        */
         public function getHue() : int {
             return $this -> hue;
         }
 
+        /**
+        * Set hue value
+        *
+        * Sets the hue value of this color
+        * @param int $hue The hue value [0, 360]
+        */
         public function setHue(int $hue) {
             $this -> hue = $hue;
         }
 
+        /**
+        * Get saturation value
+        *
+        * Returns the saturation value of this color
+        * @return int saturation value [0, 1]
+        */
         public function getSaturation() : int {
             return $this -> saturation;
         }
 
+        /**
+        * Set saturation value
+        *
+        * Sets the saturation value of this color
+        * @param int $saturation The saturation value [0, 1]
+        */
         public function setSaturation(int $saturation) {
             $this -> saturation = $saturation;
         }
 
+        /**
+        * Get value
+        *
+        * Returns the value of this color
+        * @return int value [0, 1]
+        */
         public function getValue() : int {
             return $this -> value;
         }
 
+        /**
+        * Set value
+        *
+        * Sets the value of this color
+        * @param int $value The value [0, 1]
+        */
         public function setValue(int $value) {
             $this -> value = $value;
         }
 
+
+        /**
+        * Set RGB equivalent
+        * @param int $red Red value [0, 255]
+        * @param int $red Red value [0, 255]
+        * @param int $red Red value [0, 255]
+        */
         private function setRGB($red, $green, $blue) {
             $this -> red = round($red * 255);
             $this -> green = round($green * 255);
