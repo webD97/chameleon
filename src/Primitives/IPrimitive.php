7fe7bdb2b21026f53f4982de73febf6a0ad0f992
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-15 22:18:22 +0200

    fix class path again

--- src/Primitives/IPrimitive.php
+++ src/Primitives/IPrimitive.php
@@ -1,8 +1,10 @@
 <?php
     namespace Chameleon\Primitives;
 
+    use Chameleon\Vector2;
+
     interface IPrimitive {
-        public function setPosition(\Vector2 $point);
+        public function setPosition(Vector2 $point);
         public function draw($imageResource);
     }
 ?>
