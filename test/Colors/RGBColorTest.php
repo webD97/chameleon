0220e238337c159d707c1c6f264cd59e78645a0f
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-15 18:36:13 +0200

    phpunit

---
+++ test/Colors/RGBColorTest.php
@@ -0,0 +1,9 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+
+    final class RGBColorTest extends TestCase {
+        public function testTest() {
+            $this -> assertTrue(true);
+        }
+    }
+?>
