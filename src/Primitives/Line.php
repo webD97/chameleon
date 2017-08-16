4b8c221baf879c8d11b5190fe040478cdb93f4c6
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-16 23:00:31 +0200

    add Line class

---
+++ src/Primitives/Line.php
@@ -0,0 +1,92 @@
+<?php
+    namespace Chameleon\Primitives;
+
+    use Chameleon\Image;
+    use Chameleon\Vector2;
+    use Chameleon\Colors\IColor;
+    use Chameleon\Colors\RGBColor;
+    use Chameleon\Primitives\IPrimitive;
+
+    class Line implements IPrimitive {
+        private $start;
+        private $end;
+        private $color;
+        private $thickness;
+
+        public function __construct(Vector2 $start, Vector2 $end) {
+            $this -> start = $start;
+            $this -> end = $end;
+
+            $this -> thickness = 1;
+            $this -> color = new RGBColor(0, 0, 0);
+        }
+
+        public function getStart() : Vector2 {
+            return $this -> start;
+        }
+
+        public function setStart(Vector2 $start) : self {
+            $this -> start = $start;
+            
+            return $this;
+        }
+
+        public function getEnd() : Vector2 {
+            return $this -> end;
+        }
+
+        public function setEnd(Vector2 $end) : self {
+            $this -> end = $end;
+            
+            return $this;
+        }
+
+        public function getThickness() : int {
+            return $this -> thickness;
+        }
+
+        public function setThickness(int $thickness) : self {
+            $this -> thickness = $thickness;
+            
+            return $this;
+        }
+
+        public function getColor() : IColor {
+            return $this -> color;
+        }
+
+        public function setColor(IColor $color) : self {
+            $this -> color = $color;
+            
+            return $this;
+        }
+
+        public function setPosition(Vector2 $position) : self {
+            $dx = $this -> start -> getX() - $this -> end -> getX();
+            $dy = $this -> start -> getY() - $this -> end -> getY();
+            $this -> start -> setX($position -> getX());
+            $this -> start -> setY($position -> getY());
+            $this -> end -> setX($position -> getX() - $dx);
+            $this -> end -> setY($position -> getY() - $dy);
+
+            return $this;
+        }
+
+        public function draw(Image $image) {
+            $image -> registerColorIfUnknown($this -> color);
+            
+            imagesetthickness($image -> getImageResource(), $this -> getThickness());
+
+            imageline(
+                $image -> getImageResource(), 
+                $this -> getStart() -> getX(),
+                $this -> getStart() -> getY(),
+                $this -> getEnd() -> getX(),
+                $this -> getEnd() -> getY() ,
+                $image -> getRegisteredColor($this -> getColor())
+            );
+
+            imagesetthickness($image -> getImageResource(), 1);
+        }
+    }
+?>
---
+++ test/Primitives/LineTest.php
@@ -0,0 +1,73 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+
+    use Chameleon\Primitives\Line;
+    use Chameleon\Colors\RGBColor;
+    use Chameleon\Vector2;
+
+    final class LineTest extends TestCase {
+
+        private $line;
+        private $start;
+        private $end;
+
+        protected function setUp() {
+            $this -> start = new Vector2(0, 0);
+            $this -> end = new Vector2(10, 10);
+            $this -> line = new Line($this -> start, $this -> end);
+        }
+
+        public function test__construct() {
+            $this -> assertObjectHasAttribute("start", $this -> line);
+            $this -> assertObjectHasAttribute("end", $this -> line);
+            $this -> assertObjectHasAttribute("thickness", $this -> line);
+            $this -> assertObjectHasAttribute("color", $this -> line);
+        }
+
+        public function testGetStart() {
+            $this -> assertEquals($this -> start, $this -> line -> getStart());
+        }
+
+        public function testSetStart() {
+            $start = new Vector2(5, 5);
+            $this -> line -> setStart($start);
+            $this -> assertEquals($start, $this -> line -> getStart());
+        }
+
+        public function testGetEnd() {
+            $this -> assertEquals($this -> end, $this -> line -> getEnd());
+        }
+
+        public function testSetEnd() {
+            $end = new Vector2(15, 15);
+            $this -> line -> setEnd($end);
+            $this -> assertEquals($end, $this -> line -> getEnd());
+        }
+
+        public function testGetThickness() {
+            $this -> assertEquals(1, $this -> line -> getThickness());
+        }
+
+        public function testSetThickness() {
+            $this -> line -> setThickness(5);
+            $this -> assertEquals(5, $this -> line -> getThickness());
+        }
+
+        public function testGetColor() {
+            $this -> assertEquals(new RGBColor(0, 0, 0), $this -> line -> getColor());
+        }
+
+        public function testSetColor() {
+            $color = new RGBColor(255, 255, 255);
+            $this -> line -> setColor($color);
+            $this -> assertEquals($color, $this -> line -> getColor());
+        }
+
+        public function testSetPosition() {
+            $this -> line -> setPosition(new Vector2(50, 50));
+
+            $this -> assertEquals(new Vector2(50, 50), $this -> line -> getStart());
+            $this -> assertEquals(new Vector2(60, 60), $this -> line -> getEnd());
+        }
+    }
+?>
