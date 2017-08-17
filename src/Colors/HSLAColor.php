1b1ad85d0509c9049a91794aec2a26a24f159515
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-17 19:12:07 +0200

    add HSLA colors

---
+++ src/Colors/HSLAColor.php
@@ -0,0 +1,64 @@
+<?php
+    namespace Chameleon\Colors;
+
+    use Chameleon\Exceptions\ValueOutOfBoundsException;
+    use Chameleon\Colors\HSLColor;
+
+    class HSLAColor extends HSLColor {
+        /**
+         * The alpha channel value [0, 127]
+         *
+         * @var int
+         */
+        private $alpha;
+
+        /**
+         * Class constructor
+         *
+         * @param int $red RGB red channel value [0, 255]
+         * @param int $green RGB green channel value [0, 255]
+         * @param int $blue RGB blue channel value [0, 255]
+         * @param int $alpha RGB alpha channel value [0, 127]
+         */
+        public function __construct(int $hue, float $saturation, float $lightness, float $alpha) {
+            $this -> setHue($hue);
+            $this -> setSaturation($saturation);
+            $this -> setLightness($lightness);
+            $this -> setAlpha($alpha);
+        }
+
+        /**
+         * Get string representation of this color
+         * 
+         * Format: hsla(hue, saturation, lightness, alpha)
+         *
+         * @return string
+         */
+        public function __toString() : string {
+            return "hsla(" . $this -> getHue() . ", " . $this -> getSaturation() . ", " . $this -> getLightness() . ", " . $this -> getAlpha() . ")";
+        }
+
+        /**
+         * Get RGB alpha channel value
+         *
+         * @return int RGB alpha channel avalue [0, 127]
+         */
+        public function getAlpha() : int {
+            return $this -> alpha;
+        }
+
+        /**
+         * Set RGB alpha channel value
+         *
+         * @param int $alpha RGB alpha channel avalue [0, 127]
+         * @return void
+         */
+        public function setAlpha(int $alpha) {
+            if (!$this -> checkRange($alpha, 0, 127)) {
+                throw new ValueOutOfBoundsException("alpha", $alpha, 0, 127);
+            }
+
+            $this -> alpha = $alpha;
+        }
+    }
+?>
---
+++ test/Colors/HSLAColorTest.php
@@ -0,0 +1,42 @@
+<?php
+    use PHPUnit\Framework\TestCase;
+
+    use Chameleon\Colors\HSLAColor;
+    use Chameleon\Exceptions\ValueOutOfBoundsException;
+
+    final class HSLAColorTest extends TestCase {
+
+        private $color;
+
+        protected function setUp() {
+            $this -> color = new HSLAColor(200, 1, 0.63, 16);
+        }
+
+        public function test__construct() {
+            $this -> assertObjectHasAttribute("alpha", $this -> color);
+        }
+
+        public function test__toString() {
+            $this -> assertEquals("hsla(200, 1, 0.63, 16)", $this -> color -> __toString());
+        }
+
+        public function testGetAlpha() {
+            $this -> assertEquals(16, $this -> color -> getAlpha());
+        }
+
+        public function testSetAlpha() {
+            $this -> color -> setAlpha(0);
+            $this -> assertEquals(0, $this -> color -> getAlpha());
+        }
+
+        public function testSetAlphaTooHigh() {
+            $this -> expectException(ValueOutOfBoundsException::class);
+            $this -> color -> setAlpha(128);
+        }
+
+        public function testSetAlphaTooLow() {
+            $this -> expectException(ValueOutOfBoundsException::class);
+            $this -> color -> setAlpha(-1);
+        }
+    }
+?>
