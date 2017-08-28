cfd1144fc55fe4251809a2e5c4c26c91a6d5f14a
Christian Danscheid <christian.danscheid@outlook.de>
2017-08-28 19:57:34 +0200

    add css color names

--- src/ColorFactory.php
+++ src/ColorFactory.php
@@ -8,48 +8,596 @@
             return new Hex8Color("#0000007F");
         }
 
-        public static function aliceBlue() : Hex8Color {
+        public static function AliceBlue() : Hex8Color {
             return new Hex8Color("#F0F8FF00");
         }
-
-        public static function antiqueWhite() : Hex8Color {
+        
+        public static function AntiqueWhite() : Hex8Color {
             return new Hex8Color("#FAEBD700");
         }
-
-        public static function aqua() : Hex8Color {
+        
+        public static function Aqua() : Hex8Color {
             return new Hex8Color("#00FFFF00");
         }
-
-        public static function aquamarine() : Hex8Color {
+        
+        public static function Aquamarine() : Hex8Color {
             return new Hex8Color("#7FFFD400");
         }
-
-        public static function azure() : Hex8Color {
+        
+        public static function Azure() : Hex8Color {
             return new Hex8Color("#F0FFFF00");
         }
-
-        public static function beige() : Hex8Color {
+        
+        public static function Beige() : Hex8Color {
             return new Hex8Color("#F5F5DC00");
         }
-
-        public static function bisque() : Hex8Color {
+        
+        public static function Bisque() : Hex8Color {
             return new Hex8Color("#FFE4C400");
         }
-
-        public static function black() : Hex8Color {
+        
+        public static function Black() : Hex8Color {
             return new Hex8Color("#00000000");
         }
-
-        public static function blanchedAlmond() : Hex8Color {
+        
+        public static function BlanchedAlmond() : Hex8Color {
             return new Hex8Color("#FFEBCD00");
         }
-
-        public static function blue() : Hex8Color {
+        
+        public static function Blue() : Hex8Color {
             return new Hex8Color("#0000FF00");
         }
-
-        public static function blueViolet() : Hex8Color {
-            return new Hex8Color("#8A2BE2");
+        
+        public static function BlueViolet() : Hex8Color {
+            return new Hex8Color("#8A2BE200");
+        }
+        
+        public static function Brown() : Hex8Color {
+            return new Hex8Color("#A52A2A00");
+        }
+        
+        public static function BurlyWood() : Hex8Color {
+            return new Hex8Color("#DEB88700");
+        }
+        
+        public static function CadetBlue() : Hex8Color {
+            return new Hex8Color("#5F9EA000");
+        }
+        
+        public static function Chartreuse() : Hex8Color {
+            return new Hex8Color("#7FFF0000");
+        }
+        
+        public static function Chocolate() : Hex8Color {
+            return new Hex8Color("#D2691E00");
+        }
+        
+        public static function Coral() : Hex8Color {
+            return new Hex8Color("#FF7F5000");
+        }
+        
+        public static function CornflowerBlue() : Hex8Color {
+            return new Hex8Color("#6495ED00");
+        }
+        
+        public static function Cornsilk() : Hex8Color {
+            return new Hex8Color("#FFF8DC00");
+        }
+        
+        public static function Crimson() : Hex8Color {
+            return new Hex8Color("#DC143C00");
+        }
+        
+        public static function Cyan() : Hex8Color {
+            return new Hex8Color("#00FFFF00");
+        }
+        
+        public static function DarkBlue() : Hex8Color {
+            return new Hex8Color("#00008B00");
+        }
+        
+        public static function DarkCyan() : Hex8Color {
+            return new Hex8Color("#008B8B00");
+        }
+        
+        public static function DarkGoldenRod() : Hex8Color {
+            return new Hex8Color("#B8860B00");
+        }
+        
+        public static function DarkGray() : Hex8Color {
+            return new Hex8Color("#A9A9A900");
+        }
+        
+        public static function DarkGrey() : Hex8Color {
+            return new Hex8Color("#A9A9A900");
+        }
+        
+        public static function DarkGreen() : Hex8Color {
+            return new Hex8Color("#00640000");
+        }
+        
+        public static function DarkKhaki() : Hex8Color {
+            return new Hex8Color("#BDB76B00");
+        }
+        
+        public static function DarkMagenta() : Hex8Color {
+            return new Hex8Color("#8B008B00");
+        }
+        
+        public static function DarkOliveGreen() : Hex8Color {
+            return new Hex8Color("#556B2F00");
+        }
+        
+        public static function DarkOrange() : Hex8Color {
+            return new Hex8Color("#FF8C0000");
+        }
+        
+        public static function DarkOrchid() : Hex8Color {
+            return new Hex8Color("#9932CC0000");
+        }
+        
+        public static function DarkRed() : Hex8Color {
+            return new Hex8Color("#8B00000000");
+        }
+        
+        public static function DarkSalmon() : Hex8Color {
+            return new Hex8Color("#E9967A00");
+        }
+        
+        public static function DarkSeaGreen() : Hex8Color {
+            return new Hex8Color("#8FBC8F00");
+        }
+        
+        public static function DarkSlateBlue() : Hex8Color {
+            return new Hex8Color("#483D8B00");
+        }
+        
+        public static function DarkSlateGray() : Hex8Color {
+            return new Hex8Color("#2F4F4F00");
+        }
+        
+        public static function DarkSlateGrey() : Hex8Color {
+            return new Hex8Color("#2F4F4F00");
+        }
+        
+        public static function DarkTurquoise() : Hex8Color {
+            return new Hex8Color("#00CED100");
+        }
+        
+        public static function DarkViolet() : Hex8Color {
+            return new Hex8Color("#9400D300");
+        }
+        
+        public static function DeepPink() : Hex8Color {
+            return new Hex8Color("#FF149300");
+        }
+        
+        public static function DeepSkyBlue() : Hex8Color {
+            return new Hex8Color("#00BFFF00");
+        }
+        
+        public static function DimGray() : Hex8Color {
+            return new Hex8Color("#69696900");
+        }
+        
+        public static function DimGrey() : Hex8Color {
+            return new Hex8Color("#69696900");
+        }
+        
+        public static function DodgerBlue() : Hex8Color {
+            return new Hex8Color("#1E90FF00");
+        }
+        
+        public static function FireBrick() : Hex8Color {
+            return new Hex8Color("#B2222200");
+        }
+        
+        public static function FloralWhite() : Hex8Color {
+            return new Hex8Color("#FFFAF000");
+        }
+        
+        public static function ForestGreen() : Hex8Color {
+            return new Hex8Color("#228B2200");
+        }
+        
+        public static function Fuchsia() : Hex8Color {
+            return new Hex8Color("#FF00FF00");
+        }
+        
+        public static function Gainsboro() : Hex8Color {
+            return new Hex8Color("#DCDCDC00");
+        }
+        
+        public static function GhostWhite() : Hex8Color {
+            return new Hex8Color("#F8F8FF00");
+        }
+        
+        public static function Gold() : Hex8Color {
+            return new Hex8Color("#FFD70000");
+        }
+        
+        public static function GoldenRod() : Hex8Color {
+            return new Hex8Color("#DAA52000");
+        }
+        
+        public static function Gray() : Hex8Color {
+            return new Hex8Color("#80808000");
+        }
+        
+        public static function Grey() : Hex8Color {
+            return new Hex8Color("#80808000");
+        }
+        
+        public static function Green() : Hex8Color {
+            return new Hex8Color("#00800000");
+        }
+        
+        public static function GreenYellow() : Hex8Color {
+            return new Hex8Color("#ADFF2F00");
+        }
+        
+        public static function HoneyDew() : Hex8Color {
+            return new Hex8Color("#F0FFF000");
+        }
+        
+        public static function HotPink() : Hex8Color {
+            return new Hex8Color("#FF69B400");
+        }
+        
+        public static function IndianRed() : Hex8Color {
+            return new Hex8Color("#CD5C5C00");
+        }
+        
+        public static function Indigo() : Hex8Color {
+            return new Hex8Color("#4B008200");
+        }
+        
+        public static function Ivory() : Hex8Color {
+            return new Hex8Color("#FFFFF000");
+        }
+        
+        public static function Khaki() : Hex8Color {
+            return new Hex8Color("#F0E68C00");
+        }
+        
+        public static function Lavender() : Hex8Color {
+            return new Hex8Color("#E6E6FA00");
+        }
+        
+        public static function LavenderBlush() : Hex8Color {
+            return new Hex8Color("#FFF0F500");
+        }
+        
+        public static function LawnGreen() : Hex8Color {
+            return new Hex8Color("#7CFC0000");
+        }
+        
+        public static function LemonChiffon() : Hex8Color {
+            return new Hex8Color("#FFFACD00");
+        }
+        
+        public static function LightBlue() : Hex8Color {
+            return new Hex8Color("#ADD8E600");
+        }
+        
+        public static function LightCoral() : Hex8Color {
+            return new Hex8Color("#F0808000");
+        }
+        
+        public static function LightCyan() : Hex8Color {
+            return new Hex8Color("#E0FFFF00");
+        }
+        
+        public static function LightGoldenRodYellow() : Hex8Color {
+            return new Hex8Color("#FAFAD200");
+        }
+        
+        public static function LightGray() : Hex8Color {
+            return new Hex8Color("#D3D3D300");
+        }
+        
+        public static function LightGrey() : Hex8Color {
+            return new Hex8Color("#D3D3D300");
+        }
+        
+        public static function LightGreen() : Hex8Color {
+            return new Hex8Color("#90EE9000");
+        }
+        
+        public static function LightPink() : Hex8Color {
+            return new Hex8Color("#FFB6C100");
+        }
+        
+        public static function LightSalmon() : Hex8Color {
+            return new Hex8Color("#FFA07A00");
+        }
+        
+        public static function LightSeaGreen() : Hex8Color {
+            return new Hex8Color("#20B2AA00");
+        }
+        
+        public static function LightSkyBlue() : Hex8Color {
+            return new Hex8Color("#87CEFA00");
+        }
+        
+        public static function LightSlateGray() : Hex8Color {
+            return new Hex8Color("#77889900");
+        }
+        
+        public static function LightSlateGrey() : Hex8Color {
+            return new Hex8Color("#77889900");
+        }
+        
+        public static function LightSteelBlue() : Hex8Color {
+            return new Hex8Color("#B0C4DE00");
+        }
+        
+        public static function LightYellow() : Hex8Color {
+            return new Hex8Color("#FFFFE000");
+        }
+        
+        public static function Lime() : Hex8Color {
+            return new Hex8Color("#00FF0000");
+        }
+        
+        public static function LimeGreen() : Hex8Color {
+            return new Hex8Color("#32CD3200");
+        }
+        
+        public static function Linen() : Hex8Color {
+            return new Hex8Color("#FAF0E600");
+        }
+        
+        public static function Magenta() : Hex8Color {
+            return new Hex8Color("#FF00FF00");
+        }
+        
+        public static function Maroon() : Hex8Color {
+            return new Hex8Color("#80000000");
+        }
+        
+        public static function MediumAquaMarine() : Hex8Color {
+            return new Hex8Color("#66CDAA00");
+        }
+        
+        public static function MediumBlue() : Hex8Color {
+            return new Hex8Color("#0000CD00");
+        }
+        
+        public static function MediumOrchid() : Hex8Color {
+            return new Hex8Color("#BA55D300");
+        }
+        
+        public static function MediumPurple() : Hex8Color {
+            return new Hex8Color("#9370DB00");
+        }
+        
+        public static function MediumSeaGreen() : Hex8Color {
+            return new Hex8Color("#3CB37100");
+        }
+        
+        public static function MediumSlateBlue() : Hex8Color {
+            return new Hex8Color("#7B68EE00");
+        }
+        
+        public static function MediumSpringGreen() : Hex8Color {
+            return new Hex8Color("#00FA9A00");
+        }
+        
+        public static function MediumTurquoise() : Hex8Color {
+            return new Hex8Color("#48D1CC00");
+        }
+        
+        public static function MediumVioletRed() : Hex8Color {
+            return new Hex8Color("#C7158500");
+        }
+        
+        public static function MidnightBlue() : Hex8Color {
+            return new Hex8Color("#19197000");
+        }
+        
+        public static function MintCream() : Hex8Color {
+            return new Hex8Color("#F5FFFA00");
+        }
+        
+        public static function MistyRose() : Hex8Color {
+            return new Hex8Color("#FFE4E100");
+        }
+        
+        public static function Moccasin() : Hex8Color {
+            return new Hex8Color("#FFE4B500");
+        }
+        
+        public static function NavajoWhite() : Hex8Color {
+            return new Hex8Color("#FFDEAD00");
+        }
+        
+        public static function Navy() : Hex8Color {
+            return new Hex8Color("#00008000");
+        }
+        
+        public static function OldLace() : Hex8Color {
+            return new Hex8Color("#FDF5E600");
+        }
+        
+        public static function Olive() : Hex8Color {
+            return new Hex8Color("#80800000");
+        }
+        
+        public static function OliveDrab() : Hex8Color {
+            return new Hex8Color("#6B8E2300");
+        }
+        
+        public static function Orange() : Hex8Color {
+            return new Hex8Color("#FFA50000");
+        }
+        
+        public static function OrangeRed() : Hex8Color {
+            return new Hex8Color("#FF450000");
+        }
+        
+        public static function Orchid() : Hex8Color {
+            return new Hex8Color("#DA70D600");
+        }
+        
+        public static function PaleGoldenRod() : Hex8Color {
+            return new Hex8Color("#EEE8AA00");
+        }
+        
+        public static function PaleGreen() : Hex8Color {
+            return new Hex8Color("#98FB9800");
+        }
+        
+        public static function PaleTurquoise() : Hex8Color {
+            return new Hex8Color("#AFEEEE00");
+        }
+        
+        public static function PaleVioletRed() : Hex8Color {
+            return new Hex8Color("#DB709300");
+        }
+        
+        public static function PapayaWhip() : Hex8Color {
+            return new Hex8Color("#FFEFD500");
+        }
+        
+        public static function PeachPuff() : Hex8Color {
+            return new Hex8Color("#FFDAB900");
+        }
+        
+        public static function Peru() : Hex8Color {
+            return new Hex8Color("#CD853F00");
+        }
+        
+        public static function Pink() : Hex8Color {
+            return new Hex8Color("#FFC0CB00");
+        }
+        
+        public static function Plum() : Hex8Color {
+            return new Hex8Color("#DDA0DD00");
+        }
+        
+        public static function PowderBlue() : Hex8Color {
+            return new Hex8Color("#B0E0E600");
+        }
+        
+        public static function Purple() : Hex8Color {
+            return new Hex8Color("#80008000");
+        }
+        
+        public static function RebeccaPurple() : Hex8Color {
+            return new Hex8Color("#66339900");
+        }
+        
+        public static function Red() : Hex8Color {
+            return new Hex8Color("#FF000000");
+        }
+        
+        public static function RosyBrown() : Hex8Color {
+            return new Hex8Color("#BC8F8F00");
+        }
+        
+        public static function RoyalBlue() : Hex8Color {
+            return new Hex8Color("#4169E100");
+        }
+        
+        public static function SaddleBrown() : Hex8Color {
+            return new Hex8Color("#8B451300");
+        }
+        
+        public static function Salmon() : Hex8Color {
+            return new Hex8Color("#FA807200");
+        }
+        
+        public static function SandyBrown() : Hex8Color {
+            return new Hex8Color("#F4A46000");
+        }
+        
+        public static function SeaGreen() : Hex8Color {
+            return new Hex8Color("#2E8B5700");
+        }
+        
+        public static function SeaShell() : Hex8Color {
+            return new Hex8Color("#FFF5EE00");
+        }
+        
+        public static function Sienna() : Hex8Color {
+            return new Hex8Color("#A0522D00");
+        }
+        
+        public static function Silver() : Hex8Color {
+            return new Hex8Color("#C0C0C000");
+        }
+        
+        public static function SkyBlue() : Hex8Color {
+            return new Hex8Color("#87CEEB00");
+        }
+        
+        public static function SlateBlue() : Hex8Color {
+            return new Hex8Color("#6A5ACD00");
+        }
+        
+        public static function SlateGray() : Hex8Color {
+            return new Hex8Color("#70809000");
+        }
+        
+        public static function SlateGrey() : Hex8Color {
+            return new Hex8Color("#70809000");
+        }
+        
+        public static function Snow() : Hex8Color {
+            return new Hex8Color("#FFFAFA00");
+        }
+        
+        public static function SpringGreen() : Hex8Color {
+            return new Hex8Color("#00FF7F00");
+        }
+        
+        public static function SteelBlue() : Hex8Color {
+            return new Hex8Color("#4682B400");
+        }
+        
+        public static function Tan() : Hex8Color {
+            return new Hex8Color("#D2B48C00");
+        }
+        
+        public static function Teal() : Hex8Color {
+            return new Hex8Color("#00808000");
+        }
+        
+        public static function Thistle() : Hex8Color {
+            return new Hex8Color("#D8BFD800");
+        }
+        
+        public static function Tomato() : Hex8Color {
+            return new Hex8Color("#FF634700");
+        }
+        
+        public static function Turquoise() : Hex8Color {
+            return new Hex8Color("#40E0D000");
+        }
+        
+        public static function Violet() : Hex8Color {
+            return new Hex8Color("#EE82EE00");
+        }
+        
+        public static function Wheat() : Hex8Color {
+            return new Hex8Color("#F5DEB300");
+        }
+        
+        public static function White() : Hex8Color {
+            return new Hex8Color("#FFFFFF00");
+        }
+        
+        public static function WhiteSmoke() : Hex8Color {
+            return new Hex8Color("#F5F5F500");
+        }
+        
+        public static function Yellow() : Hex8Color {
+            return new Hex8Color("#FFFF0000");
+        }
+        
+        public static function YellowGreen() : Hex8Color {
+            return new Hex8Color("#9ACD3200");
         }
     }
 ?>
